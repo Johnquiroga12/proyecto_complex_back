f4fd59de2ed6f8910fa87ae16e622d23a1b7cb27
Johnquiroga12 <100298882+Johnquiroga12@users.noreply.github.com>
2023-04-25 08:08:10 -0500

    base
    base

---
+++ src/main/java/com/examen/proyecto_complex_back/ProyectoComplexBackApplication.java
@@ -0,0 +1,13 @@
+package com.examen.proyecto_complex_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoComplexBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoComplexBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examen/proyecto_complex_back/ProyectoComplexBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.proyecto_complex_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoComplexBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
